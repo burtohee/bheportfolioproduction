cdd2b56d9a74047d6fcf24ff331bab67fa572599
burtohee <burtohe@gmail.com>
2025-05-22 22:48:27 -0400

    Update JumpTopButton.tsx

--- src/components/JumpTopButton/JumpTopButton.tsx
+++ src/components/JumpTopButton/JumpTopButton.tsx
@@ -54,10 +54,10 @@ function JumpTopButton() {
                         >
                             <path d="M16 18v2H8v-2h8zM11 7.99V16h2V7.99h3L12 4 8 7.99h3z"></path>
                         </svg>
-                        <img
+                        {/* <img
                             src="../../../src/assets/originals/icons/goup.svg"
                             alt="goupicon"
-                        ></img>
+                        ></img> */}
                     </button>
                 </div>
             </section>
